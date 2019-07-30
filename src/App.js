c267ef5200a53aa42b750ff6c2b6227e5e1819c3
Gustavo Bento de Paula <gustavob.paula@gmail.com>
2019-07-30 18:26:51 -0300

    feat: :art: init style

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className="checkout">
+      <aside>
+        <p>Alterar forma de pagamento</p>
+        <div className="title">
+          <h1>Adicionar um novo cartão de crédito</h1>
+        </div>
+      </aside>
+      <div className="content">
+        <div className="steps">
+          <p className="step step-one">Carrinho</p>
+          <p className="step step-two">Pagamento</p>
+          <p className="step step-three">Confirmaçã</p>
+        </div>
+        <form className="payment">
+          <input type="text" placeholder="Número do cartão" required />
+          <input type="text" placeholder="Nome (igual ao cartão)" required />
+          <div className="input-group">
+            <input type="text" placeholder="Validade" required />
+            <input type="text" placeholder="CVV" required />
+          </div>
+          <select name="" id="" placeholder="Número de parcelas">
+            <option value="1" selected disabled hidden>
+              Número de parcelas
+            </option>
+            <option value="1">1x</option>
+            <option value="2">2x</option>
+            <option value="3">3x</option>
+            <option value="4">4x</option>
+            <option value="5">5x</option>
+          </select>
+        </form>
+      </div>
+    </section>
   );
 }
 
